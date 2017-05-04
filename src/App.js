afeaff7212372a90072931a617ba21c73e78f65e
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-05-04 02:04:54 -0400

    Test CircleCI deployment.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component {
     return (
       <div className='App'>
         <div className='App-header'>
-          <h2>Welcome to React</h2>
+          <h2>We are Radiohead</h2>
         </div>
         <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          We're making the music that you're listening to.
         </p>
       </div>
     )
