0ee9395edbf5e19b6acb730a0ef99a89e2989b4c
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-05-11 00:12:17 -0400

    Add service worker.

---
+++ precacheServiceWorker.js
@@ -0,0 +1,26 @@
+const pkg = require('./package.json')
+const swPrecache = require('sw-precache')
+const path = require('path')
+const fs = require('fs')
+
+const root = 'build'
+
+const options = {
+  cacheId: pkg.name,
+  root: 'build',
+  staticFileGlobs: [root + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+  stripPrefix: root,
+  navigateFallback: '/200',
+  verbose: true
+}
+
+console.log('')
+console.log('👷🏻  Precaching service worker')
+console.log('')
+
+const serviceWorkerPath = path.join(root, 'service-worker.js')
+swPrecache.generate(options).then(serviceWorkerString => {
+  // remove .html suffixes; necessary for Firebase's cleanUrls option to work.
+  const cleaned = serviceWorkerString.replace(/\.html/g, '')
+  fs.writeFileSync(serviceWorkerPath, cleaned)
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import App from './App'
 import { render } from 'react-snapshot'
+import registerServiceWorker from './registerServiceWorker'
 import './index.css'
+
+registerServiceWorker()
 
 render(
   <App />,
