c8b28fc19fc955976a24e61c58aaa392202d5e65
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-05-20 00:03:08 -0400

    Fix resizeImages build script.

--- resizeImages.js
+++ resizeImages.js
@@ -1,20 +1,24 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const sharp = require('sharp')
 
-const imagesDirectory = path.join(__dirname, 'public', 'images')
-const imageFiles = fs.readdirSync(imagesDirectory)
+const sourceDirectory = path.join(__dirname, 'src', 'images')
+const buildDirectory = path.join(__dirname, 'public', 'images')
+const imageFiles = fs.readdirSync(sourceDirectory)
+
+fs.emptyDirSync(buildDirectory)
 
 imageFiles.forEach(imageFile => {
-  const imagePath = path.join(imagesDirectory, imageFile)
+  const imagePath = path.join(sourceDirectory, imageFile)
   fs.readFile(imagePath, (err, data) => {
     if (err) throw err
     resize(imagePath, data)
   })
 })
+
 const getResizedFileName = (file, width) => {
   const parsed = path.parse(file)
-  return path.join(parsed.dir, `${parsed.name}-${width}.jpg`)
+  return path.join(buildDirectory, `${parsed.name}-${width}.jpg`)
 }
 
 const widths = [320, 400, 768, 1000, 2000]
