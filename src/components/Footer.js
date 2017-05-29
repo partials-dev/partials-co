d26ae3b24e2d00b2a1047707fb82c80573e2581b
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-05-29 15:50:44 -0400

    Hide extra header and footer stuff on small screens.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { h } from 'preact' /** @jsx h */
 const Footer = () =>
   <div class='footer full-width center-contents'>
     <span class='call-to-action'>
-      <span class='album-title'>G L O S S O L <span class='inverted-a'>A</span> L I A</span>
+      <span class='album-title hide-on-small'>G L O S S O L <span class='inverted-a'>A</span> L I A</span>
       <span class='listen-button'>
         <a href='/'>
           LISTEN ON YOUTUBE
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
   const A = <span class='inverted-a'>A</span>
   const PARTIALS = <span class='logo'>P A R T I {A} L S</span>
   return (<nav class='header full-width center-contents'>
-    <Link activeClassName='active' href='/contact'>contact</Link>
-    <a href='/'>merch</a>
+    <Link activeClassName='active' class='hide-on-small' href='/contact'>contact</Link>
+    <a href='/' class='hide-on-small'>merch</a>
     <Link activeClassName='active' href='/'>{PARTIALS}</Link>
-    <Link activeClassName='active' href='/shows'>shows</Link>
-    <a href='/'>music</a>
+    <Link activeClassName='active' href='/shows' class='hide-on-small'>shows</Link>
+    <a href='/' class='hide-on-small'>music</a>
   </nav>)
 }
 
