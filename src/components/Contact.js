1677983184c0e9e84e66f0daebd10db8cd504cbe
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-06-03 12:45:35 -0400

    Add first pass on social media icons.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 import { h } from 'preact' /** @jsx h */
 import updateTitle from '../updateTitle'
+import nbsp from '../nbsp'
 
 const Contact = props => {
   updateTitle('Contact | Partials')
-  return (<main>
-    <p>mail@partialsband.com</p>
+  return (<main class='center-contents'>
+    <div class='center-text'>
+      <h2>{nbsp('B O O K I N G,  E T C.')}</h2>
+      <p class='big-text'>mail@partialsband.com</p>
+      <div class='social-media-icons'>
+        <img class='social-media-icon' src='images/FB-f-Logo__white_29.png' />
+        <img class='social-media-icon' src='images/youtube-029.png' />
+        <img class='social-media-icon' src='images/patreon-029.png' />
+        <img class='social-media-icon' src='images/soundcloud-029.png' />
+      </div>
+    </div>
   </main>)
 }
 
--- src/components/Links.js
+++ src/components/Links.js
@@ -2,8 +2,8 @@ import nbsp from '../nbsp'
 
 const Links = [
   { href: '/shows', icon: 'event', text: nbsp('s h o w s') },
-  { href: 'https://partialsmusic.bandcamp.com/merch/', icon: 'shopping_cart', text: nbsp('s t o r e'), isExternal: true },
-  { href: 'https://www.sonicbids.com/', icon: 'email', text: nbsp('p r e s s'), isExternal: true },
+  { href: 'https://partialsmusic.bandcamp.com/', icon: 'shopping_cart', text: nbsp('s t o r e'), isExternal: true },
+  { href: 'https://www.sonicbids.com/band/partials/', icon: 'email', text: nbsp('p r e s s'), isExternal: true },
   { href: '/contact', icon: 'music_note', text: nbsp('c o n t a c t') }
 ]
 
