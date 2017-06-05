5144320dca758070286d807c1536ce1d4375cc41
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-06-05 14:48:51 -0400

    Clean up compiler warnings.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,10 +12,18 @@ const Contact = props => {
       <h2>{booking}</h2>
       <p class='big-text normal-spacing'>mail@partialsband.com</p>
       <div class='social-media-icons'>
-        <a href='https://www.facebook.com/partialcinema' target='_blank'><img class='social-media-icon' src='images/facebook-icon.png' /></a>
-        <a href='https://www.youtube.com/channel/UCQh5Sxu9Rvtaqg7Fo4xMogw' target='_blank'><img class='social-media-icon' src='images/youtube-icon.png' /></a>
-        <a href='https://www.patreon.com/partials' target='_blank'><img class='social-media-icon' src='images/patreon-icon.png' /></a>
-        <a href='https://soundcloud.com/partialsmusic' target='_blank'><img class='social-media-icon' src='images/soundcloud-icon.png' /></a>
+        <a href='https://www.facebook.com/partialcinema' target='_blank' rel='noopener noreferrer'>
+          <img class='social-media-icon' alt='facebook icon' src='images/facebook-icon.png' />
+        </a>
+        <a href='https://www.youtube.com/channel/UCQh5Sxu9Rvtaqg7Fo4xMogw' target='_blank' rel='noopener noreferrer'>
+          <img class='social-media-icon' alt='youtube icon' src='images/youtube-icon.png' />
+        </a>
+        <a href='https://www.patreon.com/partials' target='_blank' rel='noopener noreferrer'>
+          <img class='social-media-icon' alt='patreon icon' src='images/patreon-icon.png' />
+        </a>
+        <a href='https://soundcloud.com/partialsmusic' target='_blank' rel='noopener noreferrer'>
+          <img class='social-media-icon' alt='soundcloud icon' src='images/soundcloud-icon.png' />
+        </a>
       </div>
     </div>
   </main>)
