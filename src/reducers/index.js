edea782d2f806e31176ab80e40d1acd001c59e69
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-06-05 10:27:46 -0400

    Switch back from Old Plum.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,7 @@ const getId = url => {
 const defaultState = {
   slices: 7,
   imageSource: 'images/kaleidoscope-image-bw-1000.jpg',
+  // imageSource: 'images/old-plum-1000.jpg',
   xPanSpeed: -0.3,
   yPanSpeed: 0.01,
   tilePosition: {
