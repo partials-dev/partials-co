21686f9666e37122e423706dad6fdb2d61bfc00c
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-06-05 00:54:18 -0400

    Darken call to action when it's hovered.

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,11 +4,15 @@ import { Link } from 'preact-router/match'
 export default props => {
   let Tag = Link
   let target = ''
+  let rel = ''
   if (props.isExternal) {
     Tag = 'a'
     target = '_blank' // open external pages in a new tab
+    // Using target="_blank" without rel="noopener noreferrer" is a security
+    // risk: see https://mathiasbynens.github.io/rel-noopener
+    rel = 'noopener noreferrer'
   }
-  return (<Tag activeClassName='active' class='center-contents' href={props.href} target={target}>
+  return (<Tag activeClassName='active' class='center-contents' href={props.href} target={target} rel={rel}>
     <span class='link-text'>{props.text}</span>
   </Tag>)
 }
